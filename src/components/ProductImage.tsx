1cd901a3de906f1b999469c30ee533a09300a132
Pablo Firulo <pfirulo@yahoo.com>
2023-04-27 12:09:12 -0300

    Primer commit

---
+++ src/components/ProductImage.tsx
@@ -0,0 +1,32 @@
+
+import styles from '../styles/styles.module.css';
+import noImage from "../assets/no-image.jpg";
+import React,{ useContext } from 'react';
+import { ProductContext } from './ProductCard';
+
+export interface Props {
+    img?: string;
+    className?: string;
+    style?: React.CSSProperties;
+}
+
+export const ProductImage = ({ img = '', className, style}: Props) => {
+
+    const { product } = useContext(ProductContext);
+    let imgToShow: string;
+    if (img) {
+        imgToShow = img;
+    } else if (product.img) {
+        imgToShow = product.img;
+    } else {
+        imgToShow = noImage;
+    }
+
+    return (
+        <img 
+        className={`${styles.productImg} ${className}`}
+        style={style}
+        src={imgToShow} 
+        alt="Product image" />
+    )
+}
