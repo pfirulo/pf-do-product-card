1cf73c5500194c073ca2d5e5b195c4fbcc9fb3e7
Pablo Firulo <pfirulo@yahoo.com>
2023-04-29 21:16:09 -0300

    Versión 0.1.0 lista

---
+++ test/data/products.ts
@@ -0,0 +1,12 @@
+
+
+export const product1 = {
+    id: '1',
+    title: "Coffee Mug Sin imagen",
+    img: "",
+}
+export const product2 = {
+    id: '1',
+    title: "Coffee Mug Con imagen",
+    img: "./coffee-mug.png",
+}
